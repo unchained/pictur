59d10db54283b58a0bcfecde31dd06f84f59bf4c
ParallelUniv3rse <arnost@unchained.studio>
2018-02-05 16:13:16 +0100

    initial commit :)

---
+++ config/express.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const glob = require('glob');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const compress = require('compression');
+const methodOverride = require('method-override');
+
+module.exports = (app, config) => {
+  app.locals.ENV = config.env;
+  app.locals.ENV_DEVELOPMENT = config.env === 'development';
+
+  app.set('views', `${config.root}/app/views`);
+  app.set('view engine', config.viewEngine);
+
+  // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true,
+  }));
+  app.use(cookieParser());
+  app.use(compress());
+  app.use(express.static(`${config.root}/dist`));
+  app.use(methodOverride());
+
+  const controllers = glob.sync(`${config.root}/app/controllers/*.js`);
+  controllers.forEach((controller) => {
+    require(controller)(app);
+  });
+
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  if (app.get('env') === 'development') {
+    app.use((err, req, res) => {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err,
+        title: 'error',
+      });
+    });
+  }
+
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: {},
+      title: 'error',
+    });
+  });
+
+  return app;
+};
