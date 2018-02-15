8d51af383f8c78ba7e34827d380b13b1cf3dda4d
ParallelUniv3rse <arnost@unchained.studio>
2018-02-15 16:35:34 +0100

    Refactoring after merge

--- server/controllers/image.js
+++ server/controllers/image.js
@@ -1,17 +1,17 @@
 const express = require('express'),
-    formidable = require('formidable'),
-    ImageHelper = require('../models/imageHelper'),
-    router = express.Router();
+  formidable = require('formidable'),
+  ImageHelper = require('../models/imageHelper'),
+  router = express.Router();
 
 module.exports = (app) => {
   app.use('/api/image', router);
 };
 
 router.post('/upload', (req, res, next) => {
-  let form = new formidable.IncomingForm();
+  const form = new formidable.IncomingForm();
   form.parse(req);
 
-  let imageId = ImageHelper.getRandomName(10);
+  const imageId = ImageHelper.getRandomName(10);
 
   form.on('fileBegin', (name, file) => {
     ImageHelper.saveImage(file, imageId);
@@ -22,24 +22,23 @@ router.post('/upload', (req, res, next) => {
   });
 
   form.on('end', () => {
-    res.redirect('/api/image/' + imageId);
+    res.redirect(`/api/image/${imageId}`);
   });
 });
 
 // TODO: pass data to Vue.js template and you are done
 router.get('/:imageId', (req, res, next) => {
-
   try {
-    let image = ImageHelper.getImageById(req.params.imageId);
+    const image = ImageHelper.getImageById(req.params.imageId);
     res.render('imageFound', {
       pageName: 'imageFound',
-      title: "Image found",
-      image: image
+      title: 'Image found',
+      image,
     });
   } catch (e) {
     res.render('imageNotFound', {
       pageName: 'imageNotFound',
-      title: "Image not found"
+      title: 'Image not found',
     });
   }
 });
--- server/models/imageHelper.js
+++ server/models/imageHelper.js
@@ -1,46 +1,44 @@
 const config = require('../../config/config'),
-    glob = require('glob-fs')({gitignore: false}),
-    Image = require('./image'),
-    fs = require('fs');
+  glob = require('glob'),
+  Image = require('./image'),
+  path = require('path');
 
-function ImageHelper() {
+class ImageHelper {
+  // Get image by imageId
+  // TODO return Image object
+  getImageById(imageId) {
+    const images = glob.sync(path.join(config.root, `/uploads/images/${imageId}.*`));
 
+    if (images.length === 1) {
+      const imageExtension = images[0].split('.').pop();
+      return new Image(imageId, `${imageId}.${imageExtension}`);
+    } else {
+      throw new Error('Requested image was not found.');
+    }
+  }
+
+  // TODO: check if path exists
+  // TODO: create path if doesn't exist
+  // Save image
+  saveImage(file, imageId) {
+    const imageName = imageId + path.extname(file.name);
+
+    try {
+      file.path = path.join(config.root, '/uploads/images/', imageName);
+    } catch (e) {
+      throw new Error('Something went wrong during image upload.');
+    }
+  }
+
+  // Generate random string
+  getRandomName(length) {
+    let name = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      name += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return name;
+  }
 }
-
-// Get image by imageId
-// TODO return Image object
-ImageHelper.prototype.getImageById = (imageId) => {
-  let images = glob.readdirSync(`/uploads/images/${imageId}*`);
-
-  if (images.length === 1) {
-    let imageExtension = images[0].split('.').pop();
-    return new Image(imageId, imageId + '.' + imageExtension);
-  } else {
-    throw "Requested image was not found.";
-  }
-};
-
-// TODO: check if path exists
-// TODO: create path if doesn't exist
-// Save image
-ImageHelper.prototype.saveImage = (file, imageId) => {
-  let imageName = imageId + '.' + file.name.split('.').pop();
-
-  try {
-    file.path = config.root + '/uploads/images/' + imageName;
-  } catch (e) {
-    throw 'Something went wrong during image upload.';
-  }
-};
-
-// Generate random string
-ImageHelper.prototype.getRandomName = (length) => {
-  let name = "";
-  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < length; i++) {
-    name += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return name;
-};
 
 module.exports = new ImageHelper();
